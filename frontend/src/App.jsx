375fd794019b1e2c985352a237a657761be624b0
AMIT-MAKASARE <AMITMAKASARE>
2025-06-13 20:16:24 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/navbar'
+import './App.css'
+import { HeroSection } from './components/heroSection'
+
+function App() {
+  return (
+    <> 
+        <Navbar/>
+        <HeroSection/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,22 @@
+const Navbar = () => {
+    return ( 
+        <>
+        <nav className="navbar">
+          <div className="logo">
+             <img src="src\img\45175a94d5389cc06b9d40c646878c60ac4bf7ed.png" alt="" />
+          </div>
+          
+       
+        <ul className="menuList">
+          <li>MENU</li>
+          <li>LOCATION</li>
+          <li>CONTACT</li>
+          <li>ABOUT</li>
+        </ul>
+        <button className='btn '><a className='text-white' href="#">Login</a></button>
+        </nav>
+        </>
+     );
+}
+ 
+export default Navbar;
